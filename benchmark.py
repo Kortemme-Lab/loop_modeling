2a8a3b727ba94515e4ca7d4470733edd740465ec
Kale Kundert <kale@thekunderts.net>
2014-08-18 17:42:29 -0700

    Debug the launch scripts.

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,35 @@
+#!/usr/bin/env python
+
 #$ -S /usr/bin/python
-#$ -l mem_free=2G
+#$ -l mem_free=1G
 #$ -l arch=linux-x64
 #$ -l netapp=1G
 #$ -l h_rt=03:00:00
+#$ -o output
+#$ -j y
 #$ -cwd
+
+import os
+import sys; sys.path.append(os.getcwd())
+import optparse
+import subprocess
+import re
+import conf
+
+# Parse arguments (e.g. input pdb)
+
+parser = optparse.OptionParser(usage='%prog [options] <pdb>')
+parser.add_option('--fast', action='store_true', dest='fast')
+options, arguments = parser.parse_args()
+
+if len(arguments) != 1:
+    print 'Usage: benchmark.py [options] <pdb>'
+    print 
+    print 'benchmark.py: error: must specify exactly one input pdb.'
+    sys.exit(1)
+
+pdb_path = arguments[0]
+loop_path = re.sub('\.pdb(\.gz)?$', '.loop', pdb_path)
 
 # Setup LD_LIBRARY_PATH
 
@@ -17,8 +43,8 @@ except KeyError:
 
 # Build the RosettaScripts command line.
 
-rosetta_path = os.path.abspath(conf.path)
-rosetta_scripts = os.path.join(rosetta_path, 'source', 'bin', 'rosetta_scripts')
+rosetta_path = os.path.abspath(conf.rosetta)
+rosetta_scripts = os.path.join(rosetta_path, 'source', 'bin', 'rosetta_scripts.mysql.linuxgccrelease')
 rosetta_database = os.path.join(rosetta_path, 'database')
 
 rosetta_command = (
@@ -26,15 +52,17 @@ rosetta_command = (
         '-database', rosetta_database,
         '-in:file:s', pdb_path,
         '-in:file:native', pdb_path,
-        '-inout:database_mode', 'mysql',
-        '-inout:database_filename', conf.db_name,
-        '-mysql:host', conf.db_host,
-        '-mysql:user', conf.db_user,
-        '-mysql:password', conf.db_password,
-        '-mysql:port', conf.db_port,
-        '-out:use_database'
+        '-inout:dbms:mode', 'mysql',
+        '-inout:dbms:database_name', conf.db_name,
+        '-inout:dbms:user', conf.db_user,
+        '-inout:dbms:password', conf.db_password,
+        '-inout:dbms:host', conf.db_host,
+        '-inout:dbms:port', conf.db_port,
+        '-out:use_database',
         '-parser:protocol', 'loop_modeler.xml',
-        '-parser:script_vars', 'model={}'.format(pdb_tag),
+        '-parser:script_vars',
+            'loop_file={0}'.format(loop_path),
+            'fast={0}'.format('yes' if options.fast else 'no'),
 )
 # Run the benchmark.
 
--- launch.py
+++ launch.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python2
 
+import sys
+import os
 import shutil
 import subprocess
+import glob
 import conf
 
 def check_hostname():
@@ -11,7 +14,7 @@ def check_hostname():
         raise SystemExit("This script must be run on chef.")
 
 def compile_rosetta():
-    rosetta_path = os.path.abspath(conf.path)
+    rosetta_path = os.path.abspath(conf.rosetta)
 
     # Setup the compiler for the cluster.
 
@@ -30,86 +33,111 @@ def compile_rosetta():
     rosetta_headers = os.path.join(
             rosetta_path, 'source', 'external', 'dbio', 'mysql')
     
-    for header in glob.glob(mysql_headers):
-        shutil.copy(header, rosetta_headers)
+    for source_path in glob.glob(mysql_headers):
+        target_name = os.path.basename(source_path)
+        target_path = os.path.join(rosetta_headers, target_name)
+        if not os.path.exists(target_path):
+            os.symlink(source_path, target_path)
 
     # Compile rosetta.
 
     scons_path = os.path.join(rosetta_path, 'source')
 
     compile_command = 'ssh', 'iqint', '; '.join([
-            'cd "{}"'.format(scons_path),
-            'nohup nice ./scons.py bin mode=release extras=mysql'])
+            'cd "%s"' % scons_path,
+            'nohup nice ./scons.py bin -j16 mode=debug extras=mysql'])
 
     return subprocess.call(compile_command)
 
-def test_benchmark():
-    benchmark_path = os.getcwd()
-
-    test_command = 'ssh', 'iqint', '; '.join([
-        'cd "{}"'.format(benchmark_path),
-        './benchmark.py structures/1srp.pdb --fast'])
-
-    return subprocess.call(test_command)
-
-def run_benchmark(pdbs):
+def run_benchmark(pdbs, debug=False):
     pdbs = [x for x in sorted(pdbs)]
 
     # Make sure all the inputs actually exist.
 
     for pdb in pdbs:
         if not os.path.exists(pdb):
-            raise ValueError("'{}' does not exist.".format(pdb))
+            raise ValueError("'{0}' does not exist.".format(pdb))
+
+    # Wipe the output directory.
+
+    if os.path.exists('output'): shutil.rmtree('output')
+    os.mkdir('output')
 
     # Submit the benchmark to the cluster.
 
     for pdb in pdbs:
-        command = 'qsub', '-t', '1-500', 'benchmark.py', pdb
+        if debug:
+            command = (
+                    'qsub', '-q', 'short.q', '-l', 'h_rt=0:30:00',
+                    'benchmark.py', pdb, '--fast'
+            )
+        else:
+            command = (
+                    'qsub', '-t', '1-500',
+                    'benchmark.py', pdb,
+            )
+
+        print ' '.join(command)
         subprocess.call(command)
 
 
 if __name__ == '__main__':
-    import argparse
+    import optparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('benchmark', nargs='+')
-    parser.add_argument('--compile-only', '-c', action='store_true')
-    arguments = parser.parse_args()
+    # Use optparse because it's available on chef.
+
+    parser = optparse.OptionParser(usage='%prog [options] <benchmark>')
+    parser.add_option('--compile-only', '-c', action='store_true', dest='compile_only')
+    parser.add_option('--execute-only', '-x', action='store_true', dest='execute_only')
+    options, arguments = parser.parse_args()
+    benchmark = set(arguments)
+
+    if not arguments:
+        print 'Usage: launch.py [options] <full|test|mini|pdbs...>'
+        print
+        print 'launch.py: error: must specify a benchmark to launch.'
+        sys.exit(1)
 
     # Compile rosetta.
 
     check_hostname()
-    error_code = compile_rosetta()
 
-    if arguments.compile_only or error_code != 0:
-        sys.exit(error_code)
+    if not options.execute_only:
+        error_code = compile_rosetta()
+        if error_code != 0:
+            sys.exit(error_code)
+
+    if options.compile_only:
+        sys.exit(1)
 
     # Run a test job, if requested.
 
-    if 'test' in arguments.benchmark:
-        error_code = test_benchmark()
-        sys.exit(error_code)
+    if 'test' in arguments:
+        pdbs = ['structures/1srp.pdb']
+        debug = True
 
     # Run the full benchmark on the specified structures, otherwise.
 
     else:
         pdbs = set()
-        benchmark = set(arguments.benchmark)
+        debug = False
 
         if 'full' in benchmark:
             benchmark.remove('full')
-            pdbs += glob.glob('structures/*.pdb')
+            pdbs |= set(glob.glob('structures/*.pdb'))
 
         if 'mini' in benchmark:
             benchmark.remove('mini')
-            pdbs += ["structures/1c5e.pdb",
-                     "structures/1cyo.pdb",
-                     "structures/1exm.pdb",
-                     "structures/2cpl.pdb",
-                     "structures/3cla.pdb",
-                     "structures/1oth.pdb"]
+            pdbs |= set([
+                    "structures/1c5e.pdb",
+                    "structures/1cyo.pdb",
+                    "structures/1exm.pdb",
+                    "structures/2cpl.pdb",
+                    "structures/3cla.pdb",
+                    "structures/1oth.pdb"])
 
-        pdbs += benchmarks
-        error_code = run_benchmark(pdbs)
-        sys.exit(error_code)
+        pdbs |= benchmark
+
+    error_code = run_benchmark(pdbs, debug=debug)
+    sys.exit(error_code)
 
