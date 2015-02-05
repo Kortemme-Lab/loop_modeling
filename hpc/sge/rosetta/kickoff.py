197cabe7f0a10a134ad40c58404b7c70182d74dd
Kale Kundert <kale@thekunderts.net>
2015-02-04 16:24:19 -0800

    Remove the $BENCHMARK_ROOT environment variable.

--- hpc/sge/rosetta/kickoff.py
+++ hpc/sge/rosetta/kickoff.py
@@ -172,13 +172,10 @@ def run_benchmark(name, script, pdbs,
     # Submit the benchmark to the cluster.
 
     qsub_command = 'qsub',
-    
-    # Set the benchmark root path
-    qsub_command += '-v', 'BENCHMARK_PATH=%s' % os.path.normpath(os.path.join(os.path.split(os.path.abspath(__file__))[0], '..', '..', '..'))
-    
     benchmark_command = 'loop_benchmark.py', benchmark_id
 
-    if nstruct is not None: assert isinstance(nstruct, int)
+    if nstruct is not None:
+        assert isinstance(nstruct, int)
 
     if fast:
         qsub_command += '-t', '1-{0}'.format((nstruct or 10) * len(pdbs))
