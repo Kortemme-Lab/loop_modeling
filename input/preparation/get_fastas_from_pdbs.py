fb26f22b95088971fc8a636a5c5ffadc12c07e98
xingjiepan <xingjiepan@gmail.com>
2018-01-18 14:45:55 -0800

    Add fasta files for the perturbed data set

---
+++ input/preparation/get_fastas_from_pdbs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+import os
+
+import pyrosetta
+from pyrosetta import rosetta
+
+
+if __name__ == '__main__':
+
+    pyrosetta.init()
+
+    for f in os.listdir('.'):
+        if f.endswith('.pdb'):
+            pose = rosetta.core.pose.Pose()
+            rosetta.core.import_pose.pose_from_file(pose, f)
+
+            with open(f[:4] + '.fasta', 'w') as fout:
+                fout.write(f[:4] + '|A\n')
+                sequence = pose.sequence()
+
+                start = 0
+                stop = start + 80
+
+                while start < len(sequence):
+                    fout.write(sequence[start:min(stop, len(sequence))] + '\n')
+                    start = stop
+                    stop = stop + 80
+
